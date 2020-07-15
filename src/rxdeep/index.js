6f8d5963f7d3b9ecf6f45f8f0ee5ec4bb8233299
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-07-15 18:19:02 +0200

    use 'useReducer' for updating state

--- src/rxdeep/index.js
+++ src/rxdeep/index.js
@@ -1,10 +1,17 @@
 import { State } from 'rxdeep';
 import { useObservable } from 'rxjs-hooks';
 
-import { createApp, initialState } from '../common';
+import {
+  createApp,
+  initialState,
+  reducer,
+  incrementAction,
+} from '../common';
 
-const state = new State(initialState.count);
-const useCount = () => useObservable(() => state) || initialState.count;
-const useIncrement = () => () => { state.value += 1; };
+const state = new State(initialState);
+const useCount = () => useObservable(() => state.sub('count')) || initialState.count;
+const useIncrement = () => () => {
+  state.value = reducer(state.value, incrementAction);
+};
 
 export default createApp(useCount, useIncrement);
