c7a5bdce3074bac3c39fbc31b6d0235efba86206
Benno Kohrs <bennokohrs@gmail.com>
2021-06-17 07:59:49 +0200

    eslint fix

--- src/agile-ts/index.js
+++ src/agile-ts/index.js
@@ -20,12 +20,12 @@ const useCount = () => useAgile(MY_COUNT).count;
 
 const useDouble = () => () => {
   // Used the inbuilt reducer for simplicity. However, AgileTs normally doesn't need any reducers!
-  MY_COUNT.patch(reducer(MY_COUNT.copy(), doubleAction));
+  MY_COUNT.set(reducer(MY_COUNT.copy(), doubleAction));
 };
 
 const useIncrement = () => () => {
   // Used the inbuilt reducer for simplicity. However, AgileTs normally doesn't need any reducers!
-  MY_COUNT.patch(reducer(MY_COUNT.copy(), incrementAction));
+  MY_COUNT.set(reducer(MY_COUNT.copy(), incrementAction));
 };
 
 export default createApp(useCount, useIncrement, useDouble);
