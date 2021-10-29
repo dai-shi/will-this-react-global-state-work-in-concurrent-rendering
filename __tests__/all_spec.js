7dc2364af9cf9e7afec40e30120a2c4967e42e11
daishi <daishi@axlight.com>
2021-10-30 08:36:49 +0900

    change test 1 not to check pending

--- __tests__/all_spec.js
+++ __tests__/all_spec.js
@@ -34,84 +34,6 @@ const TRANSITION_REPEAT_2 = 2;
 
 names.forEach((name) => {
   describe(name, () => {
-    /*
-    describe('check with events from outside', () => {
-      let delays;
-
-      beforeAll(async () => {
-        await page.goto(`http://localhost:${port}/${name}/index.html`);
-        // wait until all counts become zero
-        await Promise.all([...Array(NUM_COMPONENTS).keys()].map(async (i) => {
-          await expect(page).toMatchElement(`.count:nth-of-type(${i + 1})`, {
-            text: '0',
-            timeout: 5 * 1000,
-          });
-        }));
-      });
-
-      it('check 1: updated properly', async () => {
-        delays = [];
-        for (let loop = 0; loop < REPEAT * COUNT_PER_DUMMY; loop += 1) {
-          const start = Date.now();
-          // click buttons three times
-          await Promise.all([
-            page.click('#remoteIncrement'),
-            page.click('#remoteIncrement'),
-            sleep(50).then(() => page.click('#remoteIncrement')), // a bit delayed
-          ]);
-          delays.push(Date.now() - start);
-        }
-        console.log(name, delays);
-        // check if all counts become REPEAT * 3
-        await Promise.all([...Array(NUM_COMPONENTS).keys()].map(async (i) => {
-          await expect(page).toMatchElement(`.count:nth-of-type(${i + 1})`, {
-            text: `${REPEAT * 3}`,
-            timeout: 10 * 1000,
-          });
-        }));
-      });
-
-      it('check 2: no tearing during update', async () => {
-        // check if there's inconsistency during update
-        // see useCheckTearing() in src/common.js
-        await expect(page.title()).resolves.not.toMatch(/TEARED/);
-      });
-
-      it('check 3: ability to interrupt render', async () => {
-        // check delays taken by clicking buttons in check1
-        // each render takes at least 20ms and there are 50 components,
-        // it triggers triple clicks, so 300ms on average.
-        const avg = delays.reduce((a, b) => a + b) / delays.length;
-        expect(avg).toBeLessThan(300);
-      });
-
-      it('check 4: proper update after interrupt', async () => {
-        // click both buttons to update local count during updating shared count
-        await Promise.all([
-          ...([...Array(COUNT_PER_DUMMY)].map(() => (
-            page.click('#remoteIncrement')
-          ))),
-          page.click('#localIncrement'),
-          ...([...Array(COUNT_PER_DUMMY)].map(() => (
-            page.click('#remoteIncrement')
-          ))),
-          page.click('#localIncrement'),
-        ]);
-        // check if all counts become REPEAT * 3 + 2
-        await Promise.all([...Array(NUM_COMPONENTS).keys()].map(async (i) => {
-          await expect(page).toMatchElement(`.count:nth-of-type(${i + 1})`, {
-            text: `${REPEAT * 3 + 2}`,
-            timeout: 5 * 1000,
-          });
-        }));
-      });
-
-      afterAll(async () => {
-        await jestPuppeteer.resetBrowser();
-      });
-    });
-    */
-
     describe('with useTransition', () => {
       let delays;
 
@@ -137,24 +59,13 @@ names.forEach((name) => {
           await sleep(100);
         }
         console.log(name, delays);
-        // wait for pending
-        await expect(page).toMatchElement('#pending', {
-          text: 'Pending...',
-          timeout: 2 * 1000,
-        });
-        // Make sure that while isPending true, previous state displayed
-        await expect(page.evaluate(() => document.querySelector('#mainCount').innerHTML)).resolves.toBe('0');
-        await expect(page.evaluate(() => document.querySelector('.count:first-of-type').innerHTML)).resolves.toBe('0');
-        await sleep(1000); // this is a magic number (better way?)
-        // wait until pending disappers
-        await expect(page).toMatchElement('#pending', {
-          text: '',
-          timeout: 10 * 1000,
-        });
-        // Main state should be changed
-        await expect(page.evaluate(() => document.querySelector('#mainCount').innerHTML)).resolves.not.toBe('0');
-        // the first one should be changed
-        await expect(page.evaluate(() => document.querySelector('.count:first-of-type').innerHTML)).resolves.not.toBe('0');
+        // check if all counts become TRANSITION_REPEAT_1
+        await Promise.all([...Array(NUM_COMPONENTS).keys()].map(async (i) => {
+          await expect(page).toMatchElement(`.count:nth-of-type(${i + 1})`, {
+            text: `${TRANSITION_REPEAT_1}`,
+            timeout: 10 * 1000,
+          });
+        }));
       });
 
       it('test 2: no tearing with transition', async () => {
@@ -189,6 +100,9 @@ names.forEach((name) => {
           text: 'Pending...',
           timeout: 2 * 1000,
         });
+        // Make sure that while isPending true, previous state displayed
+        await expect(page.evaluate(() => document.querySelector('#mainCount').innerHTML)).resolves.toBe(`${TRANSITION_REPEAT_1}`);
+        await expect(page.evaluate(() => document.querySelector('.count:first-of-type').innerHTML)).resolves.toBe(`${TRANSITION_REPEAT_1}`);
         // click normal double button
         await page.click('#normalDouble');
         // check if all counts become doubled before increment
