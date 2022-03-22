cb7b182ef499da6a6890fbb09c24ae0e193183f3
daishi <daishi@axlight.com>
2022-03-22 14:41:21 +0900

    refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-let name;
-let App;
+const name = process.env.NAME || window.location.pathname.slice(1) || 'react-redux';
 
-if (process.env.NAME) {
-  name = process.env.NAME;
-  // eslint-disable-next-line import/no-dynamic-require, global-require
-  App = require(`./${process.env.NAME}`).default;
-} else {
-  name = window.location.pathname.slice(1) || 'react-redux';
-  // eslint-disable-next-line import/no-dynamic-require, global-require
-  App = require(`./${name}`).default;
-}
+// eslint-disable-next-line import/no-dynamic-require
+const App = require(`./${name}`).default;
 
 document.title = name;
 
